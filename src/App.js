dad2579dddd56553b2bf10ce8c0acf8b31682eeb
Justinas Kairys <info@ibnz.co.uk>
2021-05-22 14:55:57 +0100

    Working Staking , Unstaking

--- src/App.js
+++ src/App.js
@@ -20,95 +20,138 @@ const App = () => {
   useEffect(() => {
     //connecting to ethereum blockchain
     const ethEnabled = async () => {
-      if (window.ethereum) {
-        // await window.ethereum.send('eth_requestAccounts');
-        await window.ethereum.request({ method: 'eth_requestAccounts' });
-        window.web3 = new Web3(window.ethereum);
-
-        //connecting to metamask
-        let web3 = window.web3;
-        const accounts = await web3.eth.getAccounts();
-        setAccount(accounts[0]);
-
-        //loading users network ID and name
-        const networkId = await web3.eth.net.getId();
-        const networkType = await web3.eth.net.getNetworkType();
-        setNetwork({ ...network, id: networkId, name: networkType });
-
-        //loading TestToken contract data
-        const testTokenData = TestToken.networks[networkId];
-        if (testTokenData) {
-          let web3 = window.web3;
-          const testToken = new web3.eth.Contract(
-            TestToken.abi,
-            testTokenData.address
-          );
-          setTestTokenContract(testToken);
-          //  fetching balance of Testtoken and storing in state
-          let testTokenBalance = await testToken.methods
-            .balanceOf(accounts[0])
-            .call();
-          let convertedBalance = window.web3.utils.fromWei(
-            testTokenBalance.toString(),
-            'Ether'
-          );
-          setUserBalance(convertedBalance);
-        } else {
-          window.alert(
-            'TestToken contract is not deployed on this network, please change to testnet'
-          );
-        }
-
-        //loading TokenStaking contract data
-        const tokenStakingData = TokenStaking.networks[networkId];
-
-        if (tokenStakingData) {
-          let web3 = window.web3;
-          const tokenStaking = new web3.eth.Contract(
-            TokenStaking.abi,
-            tokenStakingData.address
-          );
-          setTokenStakingContract(tokenStaking);
-          //  fetching total staked TokenStaking  and storing in state
-          let totalStaked = await tokenStaking.methods
-            .stakingBalance(accounts[0])
-            .call();
-          let convertedBalance = window.web3.utils.fromWei(
-            totalStaked.toString(),
-            'Ether'
-          );
-          setTotalStaked(convertedBalance);
-        } else {
-          window.alert(
-            'TokenStaking contract is not deployed on this network, please change to testnet'
-          );
-        }
-        //removing loader
-        setLoader(false);
-      } else if (!window.web3) {
-        setAccount('Metamask is not detected');
-
-        setLoader(false);
-      }
+      fetchDataFromBlockchain();
     };
 
     ethEnabled();
   }, []);
 
+  const fetchDataFromBlockchain = async () => {
+    if (window.ethereum) {
+      // await window.ethereum.send('eth_requestAccounts');
+      await window.ethereum.request({ method: 'eth_requestAccounts' });
+      window.web3 = new Web3(window.ethereum);
+
+      //connecting to metamask
+      let web3 = window.web3;
+      const accounts = await web3.eth.getAccounts();
+      setAccount(accounts[0]);
+
+      //loading users network ID and name
+      const networkId = await web3.eth.net.getId();
+      const networkType = await web3.eth.net.getNetworkType();
+      setNetwork({ ...network, id: networkId, name: networkType });
+
+      //loading TestToken contract data
+      const testTokenData = TestToken.networks[networkId];
+      if (testTokenData) {
+        let web3 = window.web3;
+        const testToken = new web3.eth.Contract(
+          TestToken.abi,
+          testTokenData.address
+        );
+        setTestTokenContract(testToken);
+        //  fetching balance of Testtoken and storing in state
+        let testTokenBalance = await testToken.methods
+          .balanceOf(accounts[0])
+          .call();
+        let convertedBalance = window.web3.utils.fromWei(
+          testTokenBalance.toString(),
+          'Ether'
+        );
+        setUserBalance(convertedBalance);
+
+        //fetching contract balance
+        //updating total staked balance
+        const temptokenStakingData = TokenStaking.networks[networkId];
+        let totalStaked = await testToken.methods
+          .balanceOf(temptokenStakingData.address)
+          .call();
+        convertedBalance = window.web3.utils.fromWei(
+          totalStaked.toString(),
+          'Ether'
+        );
+        //removing initial balance 
+        convertedBalance = convertedBalance -500000;
+
+        setTotalStaked(convertedBalance);
+      } else {
+        window.alert(
+          'TestToken contract is not deployed on this network, please change to testnet'
+        );
+      }
+
+      //loading TokenStaking contract data
+      const tokenStakingData = TokenStaking.networks[networkId];
+
+      if (tokenStakingData) {
+        let web3 = window.web3;
+        const tokenStaking = new web3.eth.Contract(
+          TokenStaking.abi,
+          tokenStakingData.address
+        );
+        setTokenStakingContract(tokenStaking);
+        //  fetching total staked TokenStaking  and storing in state
+        let myStake = await tokenStaking.methods
+          .stakingBalance(accounts[0])
+          .call();
+        let convertedBalance = window.web3.utils.fromWei(
+          myStake.toString(),
+          'Ether'
+        );
+        setMyStake(convertedBalance);
+      } else {
+        window.alert(
+          'TokenStaking contract is not deployed on this network, please change to testnet'
+        );
+      }
+      //removing loader
+      setLoader(false);
+    } else if (!window.web3) {
+      setAccount('Metamask is not detected');
+
+      setLoader(false);
+    }
+  };
+
   const inputChangeHandler = (event) => {
+    // todo !!!!!!!!!!!!!!!!    todo !!!!!!!!!!!!!!!! todo !!!!!!!!!!!!!!!! todo !!!!!!!!!!!!!!!! todo !!!!!!!!!!!!!!!! todo !!!!!!!!!!!!!!!! todo !!!!!!!!!!!!!!!!
+    //need to check if is number and 0 >
     event.preventDefault();
     setInputValue(event.target.value);
   };
 
   const stakeHandler = () => {
-    let convertToWei = window.web3.utils.toWei(inputValue, 'Ether')
-    console.log('staking: ', convertToWei);
+    setLoader(true);
+    let convertToWei = window.web3.utils.toWei(inputValue, 'Ether');
+    //aproving tokens for spending
+    testTokenContract.methods
+      .approve(tokenStakingContract._address, convertToWei)
+      .send({ from: account })
+      .on('transactionHash', (hash) => {
+        tokenStakingContract.methods
+          .stakeTokens(convertToWei)
+          .send({ from: account })
+          .on('transactionHash', (hash) => {
+            setLoader(false);
+            fetchDataFromBlockchain();
+          });
+      });
+    setLoader(false);
     setInputValue('');
   };
 
   const unStakeHandler = () => {
-    let convertToWei = window.web3.utils.toWei(inputValue, 'Ether')
-    console.log('unstaking: ', convertToWei);
+    setLoader(true);
+    // let convertToWei = window.web3.utils.toWei(inputValue, 'Ether')
+    tokenStakingContract.methods
+      .unstakeTokens()
+      .send({ from: account })
+      .on('transactionHash', (hash) => {
+        setLoader(false);
+        fetchDataFromBlockchain();
+      });
+
     setInputValue('');
   };
 
