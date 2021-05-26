099cff5c81567bd45650ce63685c300f6c55e98e
Justinas Kairys <info@ibnz.co.uk>
2021-05-26 16:44:33 +0100

    typo

--- src/App.js
+++ src/App.js
@@ -298,7 +298,7 @@ const App = () => {
               &nbsp; id: <b>{network.id}</b>
             </p>
             <p>Contract Balance: {contractBalance} TestToken (Tst) </p>
-            <p>adresas {tokenStakingContract._address}</p>
+            <p>Staking Contract address: {tokenStakingContract._address}</p>
           </div>
         </div>
       </div>
